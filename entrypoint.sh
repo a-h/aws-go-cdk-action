9dfec42baf572493335320bc5acd1640a71002f8
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-07-07 23:57:35 +0100

    add extra logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 # Build Go Lambda handlers.
 # Don't expect this command line to work on MacOS - the command line tools don't support all the options.
+echo "Building Go Lambda functions..."
 find . -type f -name "*main.go" | xargs --no-run-if-empty dirname | awk '{print "GOOS=linux GOARCH=amd64 go build -ldflags=\"-s -w\" -o "$1"/lambdaFunction "$1"/main.go\0"}' | xargs bash -c 
 
 # Execute cdk commands.
+echo "Executing CDK command..."
 $1
