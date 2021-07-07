f7a2904c442aa74cb77617e3776e634a38c22b26
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-07-07 23:50:09 +0100

    build binaries

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# Build Go Lambda handlers.
+# Don't expect this command line to work on MacOS - the command line tools don't support all the options.
+find . -type f -name "*main.go" | xargs --no-run-if-empty dirname | awk '{print "GOOS=linux GOARCH=amd64 go build -ldflags=\"-s -w\" -o "$1"/lambdaFunction "$1"/main.go\0"}' | xargs bash -c 
+
+# Execute cdk commands.
 $1
