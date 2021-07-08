c772ebe47fe1e3dfc96d116492847f631613d735
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-07-08 08:52:26 +0100

    simplify this action to just a Docker execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh -v
 
-# Build Go Lambda handlers.
-# Don't expect this command line to work on MacOS - the command line tools don't support all the options.
-echo "Building Go Lambda functions..."
-find . -type f -name "*main.go" | xargs --no-run-if-empty dirname | awk '{print "GOOS=linux GOARCH=amd64 go build -ldflags=\"-s -w\" -o "$1"/lambdaFunction "$1"/main.go\0"}' | sh
-
-# Execute cdk commands.
-echo "Executing CDK command..."
+echo "Executing command..."
 sh -c "$*"
 
-# Say complete.
 echo "Done."
